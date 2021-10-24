5b708c3f0442b5908ee33895a13384c485b2ddd3
Andrew6rant <57331134+Andrew6rant@users.noreply.github.com>
2021-10-24 19:30:57 -0400

    shuffle and clean up files

--- src/main/java/Andrew6rant/tiered/block/TestStation.java
+++ src/main/java/Andrew6rant/tiered/block/TestStation.java
@@ -1,48 +1,29 @@
 package Andrew6rant.tiered.block;
 
 import Andrew6rant.tiered.Tiered;
-import Andrew6rant.tiered.api.CustomEntityAttributes;
 import Andrew6rant.tiered.api.ModifierUtils;
-import Andrew6rant.tiered.api.PotentialAttribute;
-import com.google.common.collect.Multimap;
 import net.minecraft.block.BarrelBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BarrelBlockEntity;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.attribute.EntityAttribute;
-import net.minecraft.entity.attribute.EntityAttributeInstance;
-import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.Properties;
-import net.minecraft.state.property.Property;
-import net.minecraft.text.LiteralText;
-import net.minecraft.text.MutableText;
-import net.minecraft.text.TextColor;
-import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.util.registry.Registry;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
-
-import java.util.Objects;
-import java.util.Random;
 
 public class TestStation extends BarrelBlock {
 
@@ -80,38 +61,9 @@ public class TestStation extends BarrelBlock {
     }
     @Override
     public ActionResult onUse(BlockState blockState, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit){
-
-        //((TooltipFadeAccessor) MinecraftClient.getInstance()).getHeldItemTooltipFade(0);
-        //int heldItemTooltipFade = ((TooltipFadeAccessor) MinecraftClient.getInstance()).getHeldItemTooltipFade();
         ItemStack stack = player.getStackInHand(hand);
-        /*EntityAttributeInstance instance = player.getAttributeInstance(CustomEntityAttributes.SIZE);
-        System.out.println(instance);
-        if(!stack.isEmpty()) {
-
-            for(EquipmentSlot slot : EquipmentSlot.values()) {
-                Multimap<EntityAttribute, EntityAttributeModifier> attributeModifiers = stack.getAttributeModifiers(slot);
-                if(!attributeModifiers.isEmpty()) {
-                    Multimap<EntityAttribute, EntityAttributeModifier> test = stack.getAttributeModifiers(EquipmentSlot.MAINHAND);
-                    System.out.println(test.keySet());
-                    System.out.println(attributeModifiers);
-                    attributeModifiers.keySet().forEach(attribute -> attributeModifiers.get(attribute).forEach(modifier -> {
-                        String attributeId = Registry.ATTRIBUTE.getId(attribute).toString();
-                        String uuid = modifier.getId().toString();
-                        String name = modifier.getName();
-                        String value = String.valueOf(modifier.getValue());
-                        String operation = modifier.getOperation().name().toLowerCase();
-
-                        System.out.println(attributeId);//
-                        System.out.println(uuid);
-                        System.out.println(name);
-                        System.out.println(value);//
-                        System.out.println(operation);//
-                    }));
-                }
-
-            }
-        }*/
         BlockEntity blockEntity = world.getBlockEntity(pos);
+
         if (world.isClient) {
             return ActionResult.SUCCESS;
         }
@@ -146,10 +98,6 @@ public class TestStation extends BarrelBlock {
                 return ActionResult.CONSUME;
         }
         else if(stack.getSubNbt(Tiered.NBT_SUBTAG_KEY) != null && !player.getItemCooldownManager().isCoolingDown(stack.getItem())) {
-            //Identifier tier = new Identifier(stack.getSubNbt(Tiered.NBT_SUBTAG_KEY).getString(Tiered.NBT_SUBTAG_DATA_KEY));
-            //PotentialAttribute potentialAttribute = Tiered.ATTRIBUTE_DATA_LOADER.getItemAttributes().get(tier);
-            //stack.removeSubNbt(Tiered.NBT_SUBTAG_KEY);
-            //player.getItemCooldownManager().set(stack.getItem(), 15);
             Identifier potentialAttributeID = ModifierUtils.getWeightedAttributeIDNoDuplicates(stack);
             if(potentialAttributeID != null) {
                 stack.getOrCreateSubNbt(Tiered.NBT_SUBTAG_KEY).putString(Tiered.NBT_SUBTAG_DATA_KEY, potentialAttributeID.toString());
