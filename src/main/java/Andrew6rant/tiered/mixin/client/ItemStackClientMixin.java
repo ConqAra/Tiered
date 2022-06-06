9cc4ebd77f6b2a2e3fa3da45f8e508e1b14234d5
Andrew Grant <andrew.grant@mail.rit.edu>
2022-06-06 19:57:52 -0400

    support held item combined tooltips
    does not work for boots yet

--- src/main/java/Andrew6rant/tiered/mixin/client/ItemStackClientMixin.java
+++ src/main/java/Andrew6rant/tiered/mixin/client/ItemStackClientMixin.java
@@ -27,6 +27,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import java.awt.*;
 import java.util.*;
 import java.util.List;
 
@@ -100,19 +101,39 @@ public abstract class ItemStackClientMixin {
             method = "getTooltip",
             at = @At(value = "RETURN", target = "Ljava/util/List;add(Ljava/lang/Object;)Z"))
     private void test(PlayerEntity player, TooltipContext context, CallbackInfoReturnable<List<Text>> cir) {
-
         if (isTiered && this.hasNbt() && this.getSubNbt(Tiered.NBT_SUBTAG_KEY) != null) { // only run on tiered items
             List<Text> list = cir.getReturnValue();
             Identifier tier = new Identifier(this.getOrCreateSubNbt(Tiered.NBT_SUBTAG_KEY).getString(Tiered.NBT_SUBTAG_DATA_KEY));
             PotentialAttribute attribute = Tiered.ATTRIBUTE_DATA_LOADER.getItemAttributes().get(tier);
-            TranslatableText titleText = (TranslatableText) list.get(2);
-            int skipLine = titleText.getKey().equals("item.modifiers.mainhand") ? 4 : 2;
             list.remove(1); // remove Vanilla's blank tooltip line
+
+            TranslatableText titleText = (TranslatableText) list.get(1);
+            //int skipLine = titleText.getKey().equals("item.modifiers.mainhand") ? 4 : 2;
+            int skipLine = 2;
             Set<String> set = new HashSet<>();
             Set<TranslatableText> noDuplicates = new HashSet<>();
+/*
+            if (titleText.getKey().equals("item.modifiers.mainhand")) {
+                System.out.println("Mainhand");
+                for (int i = 2; i < 4; i++) { // translate blank TextComponents with TranslatableComponent siblings into TranslatableTexts
+                    System.out.println(list.get(i));
+                    if (!(list.get(i) instanceof TranslatableText)) {
+                        TranslatableText translatableText = (TranslatableText) list.get(i).getSiblings().get(0);
+                        list.remove(i);
+                        TranslatableText newText = (TranslatableText) translatableText.getArgs()[1];
+                        list.add(i, new TranslatableText(translatableText.getKey(), Integer.parseInt((String)translatableText.getArgs()[0]), new TranslatableText(newText.getKey())));
+                    }
+                }
+            }*/
 
             for (int i = skipLine; i < list.size(); i++) { // Skip the first few lines of tooltip
                 //System.out.println(list.get(i));
+                if (!(list.get(i) instanceof TranslatableText)) {
+                    TranslatableText translatableText = (TranslatableText) list.get(i).getSiblings().get(0);
+                    list.remove(i);
+                    TranslatableText newText = (TranslatableText) translatableText.getArgs()[1];
+                    list.add(i, new TranslatableText(translatableText.getKey(), Integer.parseInt((String)translatableText.getArgs()[0]), new TranslatableText(newText.getKey())));
+                }
                 TranslatableText listText = (TranslatableText) list.get(i);
                 Object[] args = listText.getArgs();
                 TranslatableText argText = (TranslatableText) args[1];
@@ -133,13 +154,14 @@ public abstract class ItemStackClientMixin {
                 //System.out.println(args[0] + " --- " + key);
                 //System.out.println(Arrays.toString(translatableText.getArgs()));
             }
+
+
             System.out.println("Dup"+noDuplicates);
             System.out.println("set"+set);
-            if (titleText.getKey().equals("item.modifiers.mainhand")) {
+            //if (titleText.getKey().equals("item.modifiers.mainhand")) {
                 // The first two lines of held weapons are blank TextComponents with sibling TranslatableComponents
-                list.add(3, new TranslatableText("tooltip.tiered.space", new TranslatableText("attribute.modifier.equals.0", 9, new TranslatableText("attribute.name.generic.attack_damage")).formatted(Formatting.DARK_GREEN)));
-            }
-            //list.add(new TranslatableText("tooltip.tiered.attribute.label.testing"));
+                //list.add(3, new TranslatableText("tooltip.tiered.space", new TranslatableText("attribute.modifier.equals.0", 9, new TranslatableText("attribute.name.generic.attack_damage")).formatted(Formatting.DARK_GREEN)));
+            //}
             for (int i = 0; i < noDuplicates.size(); i += 2) {
                 //list.add(text);
                 TranslatableText text = (TranslatableText) noDuplicates.toArray()[i];
