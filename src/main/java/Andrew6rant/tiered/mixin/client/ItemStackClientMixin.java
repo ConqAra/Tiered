8f9514133300641222809aedb069399b1b153326
Andrew Grant <andrew.grant@mail.rit.edu>
2023-01-06 22:58:06 -0500

    temp push for ItemStackClientMixin
    get green tooltip item replacement working properly

--- src/main/java/Andrew6rant/tiered/mixin/client/ItemStackClientMixin.java
+++ src/main/java/Andrew6rant/tiered/mixin/client/ItemStackClientMixin.java
@@ -107,6 +107,103 @@ public abstract class ItemStackClientMixin {
             method = "getTooltip",
             at = @At(value = "RETURN", target = "Ljava/util/List;add(Ljava/lang/Object;)Z"))
     private void test(PlayerEntity player, TooltipContext context, CallbackInfoReturnable<List<Text>> cir) {
+        if (isTiered && this.hasNbt() && this.getSubNbt(Tiered.NBT_SUBTAG_KEY) != null) { // only run on tiered items
+            List<Text> list = cir.getReturnValue();
+            List<Text> badlyFormattedList = new ArrayList<>();
+            List<Text> properlyFormattedList = new ArrayList<>();
+
+            list.removeIf(text -> (!(text instanceof TranslatableText) && text.getSiblings().size() == 0)); // remove blank tooltip lines
+            for (Text text : list) {
+            //    System.out.println("||||| "+text);
+            }
+            List<TranslatableText> modifierList = new ArrayList<>();
+            //boolean hasMainhand = false;
+            //boolean hasOffhand = false;
+            byte hasMainhandIndex = -1; // invalid index
+            byte hasOffhandIndex = -1;
+            // tooltip may need to be combined, e.g.
+            // When in Main Hand: ...
+            // and
+            // When in Off Hand: ...
+            // to
+            // When held:
+            // but only if main and off hand are identical
+            for (byte i = 0; i < list.size(); i++) {
+                if (list.get(i) instanceof TranslatableText translatableText) {
+                    properlyFormattedList.add(translatableText);
+                    if (translatableText.getKey().startsWith("item.modifiers.mainhand")) {
+                        hasMainhandIndex = i;
+                    }
+                    if (translatableText.getKey().startsWith("item.modifiers.offhand")) {
+                        hasOffhandIndex = i;
+                    }
+                } else {
+                    badlyFormattedList.add(list.get(i));
+                }
+            }
+            // This code replaces Vanilla's held item damage and attack speed tooltips (the green ones) with a custom one
+            if (hasMainhandIndex != -1) { // only run this code on held items
+                for (Text text : badlyFormattedList) {
+                    if (text.getSiblings() != null) {
+                        for (Text sibling : text.getSiblings()) {
+                            //System.out.println("|sibling| " + sibling);
+                            if (sibling instanceof TranslatableText translatableText) {
+                                if (translatableText.getKey().equals("attribute.modifier.equals.0")) {
+                                    list.remove(text);
+                                    list.add(hasMainhandIndex + 1, new TranslatableText(translatableText.getKey(), trailZeros(Float.parseFloat(String.valueOf(translatableText.getArgs()[0]))), new TranslatableText(((TranslatableText)translatableText.getArgs()[1]).getKey())).formatted(Formatting.DARK_GREEN));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            // if both mainhand and offhand are present, remove duplicate tooltip items
+            if (hasMainhandIndex != -1 && hasOffhandIndex != -1) {
+                //System.out.println("has both, Mainhand: "+hasMainhandIndex+" Offhand: "+hasOffhandIndex);
+                //List<Text> tempList = list.subList(hasMainhandIndex, hasOffhandIndex - 1);
+                //System.out.println("sublist 1: "+list.subList(hasMainhandIndex + 1, hasOffhandIndex - 1));
+                //System.out.println("sublist 2: "+list.subList(hasOffhandIndex + 1, list.size()));
+
+                List<Text> tempList = properlyFormattedList.subList(1, (properlyFormattedList.size() / 2) - 1);
+                List<Text> tempList2 = properlyFormattedList.subList((properlyFormattedList.size() / 2) + 1, properlyFormattedList.size() - 1);
+                //System.out.println("sublist 1: "+properlyFormattedList.subList(1, (properlyFormattedList.size() / 2) - 1));
+                //System.out.println("sublist 2: "+properlyFormattedList.subList((properlyFormattedList.size() / 2) + 1, properlyFormattedList.size() - 1));
+                System.out.println(tempList.equals(tempList2));
+
+                if (properlyFormattedList.subList(1, (properlyFormattedList.size() / 2) - 1).equals(properlyFormattedList.subList((properlyFormattedList.size() / 2) + 1, properlyFormattedList.size() - 1))) { // if both mainhand and offhand are identical
+                    System.out.println("both are identical");
+                    //list.removeAll(properlyFormattedList.subList(hasMainhandIndex, hasOffhandIndex));
+                    //list.addAll(hasMainhandIndex, tempList);
+                    //list.add(hasMainhandIndex, new TranslatableText("item.modifiers.both_hands").formatted(Formatting.GRAY));
+                }
+            }
+
+
+            /*
+            int index = -1;
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i) instanceof TranslatableText translatableText) {
+                    if (translatableText.getKey().startsWith("item.modifiers.mainhand")) {
+                        modifierList.add(translatableText);
+                        if (index == -1) {
+                            index = i;
+                        }
+                    }
+                    if (modifierList.size() > 1) {
+                        list.subList(i-1, list.size()).clear();
+                    }
+                }
+            }
+            if (modifierList.size() > 1) {
+                list.removeAll(modifierList);
+                list.add(index, new TranslatableText("item.modifiers.both_hands").formatted(Formatting.GRAY));
+            }
+            */
+            for (Text text : list) {
+            //    System.out.println(text);
+            }
+            System.out.println("------");
+        }
         /*
 
         if (isTiered && this.hasNbt() && this.getSubNbt(Tiered.NBT_SUBTAG_KEY) != null) { // only run on tiered items
